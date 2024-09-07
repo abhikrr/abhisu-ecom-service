a0409d63c0b2dde3531d29a8af658bc56d556f09
abhikrr <abhik.raushan@gmail.com>
2024-09-07 15:19:30 +0530

    Initial Commit
    Initial Commit

---
+++ src/main/java/com/abhisu/ecom/AbhisuEcomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.abhisu.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbhisuEcomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbhisuEcomServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhisu/ecom/AbhisuEcomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhisu.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbhisuEcomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
