54360dc8c82d9231afd5f02716719490aa34d092
abhikrr <abhik.raushan@gmail.com>
2024-10-28 16:07:48 +0530

    Update TestController.java

--- src/main/java/com/abhisu/ecom/controller/TestController.java
+++ src/main/java/com/abhisu/ecom/controller/TestController.java
@@ -3,20 +3,34 @@ package com.abhisu.ecom.controller;
 import com.abhisu.ecom.dto.ResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
-    @GetMapping("/hello-world")
-    public ResponseEntity<ResponseDTO> helloWorld(){
-        return new ResponseEntity<>(new ResponseDTO(HttpStatus.OK.value(), "Hello World!!"), HttpStatus.OK);
+    @GetMapping
+    public ResponseEntity<ResponseDTO> testGetApi(){
+        return new ResponseEntity<>(new ResponseDTO(HttpStatus.OK.value(), "This is a test of Get API Controller"), HttpStatus.OK);
     }
 
-    @GetMapping("/")
-    public String showHome(){
-        return "home";
+    @PostMapping
+    public ResponseEntity<ResponseDTO> testPostApi(){
+        return new ResponseEntity<>(new ResponseDTO(HttpStatus.CREATED.value(), "This is a test Post API Controller"), HttpStatus.CREATED);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseDTO> testPutApi(){
+        return new ResponseEntity<>(new ResponseDTO(HttpStatus.OK.value(), "This is a test of Put API Controller"), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ResponseDTO> testPatchApi(){
+        return new ResponseEntity<>(new ResponseDTO(HttpStatus.OK.value(), "This is a test of Patch API Controller"), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseDTO> testDeleteApi(){
+        return new ResponseEntity<>(new ResponseDTO(HttpStatus.OK.value(), "This is a test of Delete API Controller"), HttpStatus.OK);
+    }
 }
